78eefbf46337bcda3998b3018da5611ce5598d9a
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2023-12-08 15:46:26 -0800

    add side bar, project display and project context store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import './App.css';
+import { useState } from "react";
+import SideBar from "./components/SideBar";
+import Project from "./components/Project";
+import { dummyProjects } from "./models/dummy-projects";
+import { ProjectContext } from "./store/project-context";
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+    const [projects, setProjects] = useState(dummyProjects);
+    const [selectedProject, setSelectedProject] = useState();
+
+    function addProject(newProject) {
+        newProject.id = projects.length + 1;
+        console.log("new project: " + newProject.id);
+        setProjects([
+            ...projects,
+            newProject
+        ])
+    }
+
+    function removeProject(id) {
+        console.log("removing project id: " + id);
+        const updatedProjects = projects.filter(project => project.id != id);
+        setProjects(updatedProjects);
+    }
+
+    function showProject(id) {
+        console.log("looking for project id: " + id);
+        const foundProject = projects.find(project => project.id == id);
+        console.log("found project: " + foundProject.id);
+        setSelectedProject(foundProject);
+    }
+
+    const projectContextValue = {
+        projects: projects,
+        addProject: addProject,
+        removeProject: removeProject
+    }
+    return (
+        <ProjectContext.Provider value={projectContextValue}>
+            <div className="flex-row">
+                <SideBar onSelectProject={showProject}></SideBar>
+                {selectedProject && <Project project={selectedProject}></Project>}
+                {!selectedProject && <div>create a project first!</div>}
+            </div>
+        </ProjectContext.Provider>
+
+    );
 }
 
 export default App;
---
+++ src/models/dummy-projects.js
@@ -0,0 +1,50 @@
+export const dummyProjects = [
+    {
+        id: 1,
+        name: "project name 1",
+        createDate: "Dec 9 2024",
+        description: "dummy project descrioption",
+        tasks: [
+            {
+                name: "task name 1",
+                description: "dummy task description"
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "project name 2",
+        createDate: "Dec 9 2024",
+        description: "dummy project descrioption",
+        tasks: [
+            {
+                name: "task name 2",
+                description: "dummy task description"
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "project name 3",
+        createDate: "Dec 9 2024",
+        description: "dummy project descrioption",
+        tasks: [
+            {
+                name: "task name 3",
+                description: "dummy task description"
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: "project name 4",
+        createDate: "Dec 9 2024",
+        description: "dummy project descrioption",
+        tasks: [
+            {
+                name: "task name 3",
+                description: "dummy task description"
+            }
+        ]
+    }
+]
